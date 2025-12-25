a0891fb9aaef7e0e30cc3c0c64e8cb3ada87f425
jstvoid <idke11895@gmail.com>
2025-12-25 05:33:11 +0300

    uefi-edk2

--- Uefi/main.c
+++ Uefi/main.c
@@ -2,15 +2,26 @@
 #include <Library/UefiLib.h>
 #include <Library/UefiBootServicesTableLib.h>
 
+
 EFI_STATUS
 EFIAPI
 UefiMain(
-	IN EFI_HANDLE ImageHandle,
-	IN EFI_SYSTEM_TABLE *SystemTable
+    IN EFI_HANDLE ImageHandle,
+    IN EFI_SYSTEM_TABLE *SystemTable
 )
 {
-	Print(L"Hello UEFI!\r\n");
-	
-	gBS->Stall(4 * 1000 * 1000);
-	return EFI_SUCCESS;
+    Print(L"Text before clearing the screen\r\n");
+    gBS->Stall(3000000);
+    
+    EFI_STATUS Status;
+    Status = SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
+    if(EFI_ERROR(Status))
+    {
+        return Status;
+    }
+    Print(L"After cleaning screen!\r\n");
+    gBS->Stall(3000000);
+    return EFI_SUCCESS;
+
+    
 }
